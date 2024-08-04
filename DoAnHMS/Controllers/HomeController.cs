afce3a012e361bca4006cff97a28f12b9c61c253
imhoangnguyen <Hoangabc000@gmail.com>
2024-08-04 18:59:25 +0700

    Day code len

---
+++ DoAnHMS/Controllers/HomeController.cs
@@ -0,0 +1,282 @@
+﻿using DoAnHMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnHMS.Controllers
+{
+    public class HomeController : BaseController
+    {
+        DoAnHMSEntities db = new DoAnHMSEntities();
+        List<CTPhieuThuePhong> cTPhieuThuePhong;
+        List<DichVu> dichVu;
+        Dictionary<string, DichVu> hm1;
+
+        public class ChartData
+        {
+            public String label;
+            public decimal value;
+        }
+
+        public class ChartDashBoard
+        {
+            public List<ChartData> data1 = new List<ChartData>();
+            public List<ChartData> data2 = new List<ChartData>();
+            public List<ChartData> data3 = new List<ChartData>();
+            public List<ChartData> data4 = new List<ChartData>();
+            public List<ChartData> data5 = new List<ChartData>();
+        }
+
+        public ActionResult Index()
+        {
+            List<string> maPDP = db.PhieuDatPhongs.Select(x => x.maPDP).ToList();
+            List<string> maPDPinPTP = db.PhieuThuePhongs.Select(x => x.maPDP).ToList();
+            for (int i = 0; i < maPDPinPTP.Count; i++)
+            {
+                string item = maPDPinPTP[i];
+                if (maPDP.Contains(item))
+                {
+                    maPDP.Remove(item);
+                }
+            }
+            //var phieuDatPhongs = db.PhieuDatPhongs
+            var lienHe = db.LienHes.Where(x => x.tinhTrang == false).ToList();
+            var phanHoi = db.PhanHois.Where(x => x.TinhTrang == false).ToList();
+            ViewBag.PDP = maPDP.Count() == 0 ? "" : maPDP.Count().ToString();
+            ViewBag.LienHe = lienHe.Count() == 0 ? "" : lienHe.Count().ToString();
+            ViewBag.PhanHoi = phanHoi.Count() == 0 ? "" : phanHoi.Count().ToString();
+
+            var chartDashBoard = new ChartDashBoard();
+
+            string query = "SELECT [maPTP] ,[maP] ,[ngaySD] ,[maDV] ,[soLuong]   FROM [dbo].[CTPhieuThuePhong]";
+            cTPhieuThuePhong = db.Database.SqlQuery<CTPhieuThuePhong>(query).ToList();
+
+
+            query = "SELECT [maDV]       ,[tenDV]       ,[gia]   FROM [dbo].[DichVu]";
+            dichVu = db.Database.SqlQuery<DichVu>(query).ToList();
+
+            hm1 = new Dictionary<string, DichVu>();
+            foreach (DichVu item in dichVu)
+            {
+                hm1[item.maDV] = item;
+            }
+
+
+
+
+
+
+            var hm = chart1();
+            var hm2 = chart2();
+            var hm3 = chart3();
+            var hm4 = chart4();
+            var hm5 = chart5();
+
+            chartDashBoard.data1 = hm.Values.ToList().OrderBy(x => x.label).ToList();
+            chartDashBoard.data2 = hm2.Values.ToList().OrderBy(x => x.label).ToList();
+            chartDashBoard.data3 = hm3.Values.ToList().OrderBy(x => x.label).ToList();
+            chartDashBoard.data4 = hm4.Values.ToList().OrderBy(x => x.label).ToList();
+            chartDashBoard.data5 = hm5.Values.ToList().OrderBy(x => x.label).ToList();
+
+
+
+
+            return View(chartDashBoard);
+        }
+
+        public Dictionary<String, ChartData> chart1()
+        {
+            var hm = new Dictionary<String, ChartData>();
+            foreach (var item in cTPhieuThuePhong)
+            {
+                var k = $"{item.ngaySD.Month}/{item.ngaySD.Year}";
+                if (hm.ContainsKey(k))
+                {
+                    var val = hm[k].value;
+                    hm[k].value = val + hm1[item.maDV].gia * item.soLuong;
+                }
+                else
+                {
+                    hm[k] = new ChartData()
+                    {
+                        label = k,
+                        value = hm1[item.maDV].gia * item.soLuong
+                    };
+
+                    var _1Month = item.ngaySD.AddMonths(1);
+                    var k1 = $"{_1Month.Month}/{_1Month.Year}";
+
+                    if (!hm.ContainsKey(k1))
+                    {
+                        hm[k1] = new ChartData()
+                        {
+                            label = k1,
+                            value = 0
+                        };
+                    }
+
+
+                    _1Month = item.ngaySD.AddMonths(-1);
+                    k1 = $"{_1Month.Month}/{_1Month.Year}";
+
+                    if (!hm.ContainsKey(k1))
+                    {
+                        hm[k1] = new ChartData()
+                        {
+                            label = k1,
+                            value = 0
+                        };
+                    }
+
+                }
+            }
+
+            return hm;
+        }
+
+        public Dictionary<String, ChartData> chart2()
+        {
+
+            var hm2 = new Dictionary<String, ChartData>();
+            foreach (var item in cTPhieuThuePhong)
+            {
+                var k = $"{item.ngaySD.Year}";
+                if (hm2.ContainsKey(k))
+                {
+                    var val = hm2[k].value;
+                    hm2[k].value = val + hm1[item.maDV].gia * item.soLuong;
+                }
+                else
+                {
+                    hm2[k] = new ChartData()
+                    {
+                        label = k,
+                        value = hm1[item.maDV].gia * item.soLuong
+                    };
+
+                    var _1 = item.ngaySD.AddYears(1);
+                    var k1 = $"{_1.Year}";
+
+                    if (!hm2.ContainsKey(k1))
+                    {
+                        hm2[k1] = new ChartData()
+                        {
+                            label = k1,
+                            value = 0
+                        };
+                    }
+
+
+                    _1 = item.ngaySD.AddYears(-1);
+                    k1 = $"{_1.Year}";
+
+                    if (!hm2.ContainsKey(k1))
+                    {
+                        hm2[k1] = new ChartData()
+                        {
+                            label = k1,
+                            value = 0
+                        };
+                    }
+
+                }
+            }
+            return hm2;
+        }
+
+
+        public Dictionary<String, ChartData> chart3()
+        {
+
+            var nv = db.NhanViens.ToList();
+
+
+            var hm = new Dictionary<String, ChartData>();
+            var x = 0;
+
+            foreach (var item in cTPhieuThuePhong)
+            {
+
+                var k = $"{nv[x % nv.Count].maNV}:{nv[x % nv.Count].tenNV}";
+                if (hm.ContainsKey(k))
+                {
+                    var _x = hm[k].value;
+                    hm[k].value = _x + hm1[item.maDV].gia * item.soLuong;
+                }
+                else
+                {
+                    hm[k] = new ChartData() { label = k, value = 0 };
+                }
+                x++;
+            }
+            return hm;
+        }
+
+
+        public Dictionary<String, ChartData> chart4()
+        {
+
+
+
+
+            var hm = new Dictionary<String, ChartData>();
+
+
+            foreach (var item in cTPhieuThuePhong)
+            {
+                var k = item.maP.Substring(0, 2);
+                k = k.Replace("P", "Tầng ");
+
+                if (hm.ContainsKey(k))
+                {
+                    var x = hm[k].value;
+                    hm[k].value = x + hm1[item.maDV].gia * item.soLuong;
+                }
+                else
+                {
+                    hm[k] = new ChartData() { label = k, value = hm1[item.maDV].gia * item.soLuong };
+                }
+            }
+            return hm;
+        }
+
+
+
+
+        public Dictionary<String, ChartData> chart5()
+        {
+
+            var _l = db.DichVus.ToList();
+            var _hm = new Dictionary<String, DichVu>();
+
+            foreach (var item in _l)
+            {
+                _hm[item.maDV] = item;
+            }
+
+
+
+            var hm = new Dictionary<String, ChartData>();
+
+
+            foreach (var item in cTPhieuThuePhong)
+            {
+                var k = $"{item.maDV}:{_hm[item.maDV].tenDV}";
+
+                if (hm.ContainsKey(k))
+                {
+                    var x = hm[k].value;
+                    hm[k].value = x + hm1[item.maDV].gia * item.soLuong;
+                }
+                else
+                {
+                    hm[k] = new ChartData() { label = k, value = hm1[item.maDV].gia * item.soLuong };
+                }
+
+            }
+            return hm;
+        }
+    }
+}
---
+++ DoAnHMS/Controllers/ThongKeController.cs
@@ -0,0 +1,165 @@
+﻿using DoAnHMS.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnHMS.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        DoAnHMSEntities db = new DoAnHMSEntities();
+        // GET: ThongKe
+        public ActionResult ByDichVu(string fromDate = "", string toDate = "")
+        
+        {
+            if(fromDate == "" || toDate == "")
+            {
+                fromDate = "2000-1-1";
+                toDate = "2050-1-1";
+            }
+            string query = "select dv.maDV, dv.tenDV, sum(dv.gia * ctptp.soLuong)" +
+                " from DichVu dv, CTPhieuThuePhong ctptp, PhieuThuePhong ptp, HoaDon hd" +
+                " where not(dv.maDV = 'DV0000') and (dv.maDV = ctptp.maDV) and (ptp.maPTP = hd.maPTP) and (ptp.maPTP = ctptp.maPTP) and (ptp.ngayThue between '" +
+                fromDate + "' and '" + toDate + "') and (ptp.ngayTra between '" +
+                fromDate + "' and '" + toDate + "')" +
+                " group by dv.maDV, dv.tenDV";
+
+            ViewBag.fromDate = fromDate;
+            ViewBag.toDate = toDate;
+            var data = db.Database.SqlQuery<DichVu>(query).ToList();
+
+            return View(data);
+        }
+
+        public class ChartData
+        {
+            public string maP;
+            public HashSet<String> listDv;
+            public int dem;
+            public decimal price;
+        }
+        public ActionResult Index()
+        {
+            string query = "SELECT [maPTP] ,[maP] ,[ngaySD] ,[maDV] ,[soLuong]   FROM [dbo].[CTPhieuThuePhong]";
+            var cTPhieuThuePhong = db.Database.SqlQuery<CTPhieuThuePhong>(query).ToList();
+
+            query = "SELECT [maDV]       ,[tenDV]       ,[gia]   FROM [dbo].[DichVu]";
+            var dichVu = db.Database.SqlQuery<DichVu>(query).ToList();
+
+            Dictionary<string, DichVu> hm = new Dictionary<string, DichVu>();
+            foreach(DichVu item in dichVu)
+            {
+                hm[item.maDV] = item;
+            }
+
+            Dictionary<string, ChartData> hm1 = new Dictionary<string, ChartData>();
+
+            foreach (CTPhieuThuePhong item in cTPhieuThuePhong)
+            {
+                if(hm1.ContainsKey(item.maP))
+                {
+                    hm1[item.maP].price += hm[item.maDV].gia;
+                }
+                else
+                {
+                    ChartData data = new ChartData()
+                    {
+                        maP = item.maP,
+                        price = hm[item.maDV].gia * item.soLuong
+                    };
+                    hm1[item.maP] = data;
+                }
+            }
+
+            return View(hm1.Values.ToList());
+        }
+
+
+
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+
+        public ActionResult ExportToCsv()
+        {
+            string query = "SELECT [maPTP] ,[maP] ,[ngaySD] ,[maDV] ,[soLuong]   FROM [dbo].[CTPhieuThuePhong]";
+            var cTPhieuThuePhong = db.Database.SqlQuery<CTPhieuThuePhong>(query).ToList();
+
+            query = "SELECT [maDV]       ,[tenDV]       ,[gia]   FROM [dbo].[DichVu]";
+            var dichVu = db.Database.SqlQuery<DichVu>(query).ToList();
+
+            Dictionary<string, DichVu> hm = new Dictionary<string, DichVu>();
+            foreach (DichVu item in dichVu)
+            {
+                hm[item.maDV] = item;
+            }
+
+            Dictionary<string, ChartData> hm1 = new Dictionary<string, ChartData>();
+
+            foreach (CTPhieuThuePhong item in cTPhieuThuePhong)
+            {
+                if (hm1.ContainsKey(item.maP))
+                {
+                    hm1[item.maP].price += hm[item.maDV].gia;
+                    hm1[item.maP].listDv.Add($"{item.maDV}:{item.soLuong}");
+                    hm1[item.maP].dem = hm1[item.maP].listDv.Count;
+                }
+                else
+                {
+                    HashSet<string> list = new HashSet<string>();
+                    list.Add($"{item.maDV}:{item.soLuong}");
+                    ChartData data = new ChartData()
+                    {
+                        maP = item.maP,
+                        listDv = list,
+                        dem = 1,
+                        price = hm[item.maDV].gia * item.soLuong
+
+                    };
+                    hm1[item.maP] = data;
+                }
+            }
+            var csvContent = new StringBuilder();
+            csvContent.AppendLine("Ma phong,Danh sach dich vu,So dich vu,Tong tien dich vu");
+            foreach (var item in hm1.Values)
+            {
+                var x = new StringBuilder();
+                foreach (var item1 in item.listDv)
+                {
+                    x.Append($"{item1} ");
+                }
+
+                csvContent.AppendLine($"{item.maP},{x.ToString().TrimEnd()},{item.dem},{item.price}");
+            }
+
+            // Set response headers
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=ExportedData.csv");
+            Response.Charset = "";
+            Response.ContentType = "text/csv";
+
+            // Write CSV content to the response stream
+            using (var sw = new StringWriter())
+            {
+                sw.Write(csvContent.ToString());
+                Response.Output.Write(sw.ToString());
+            }
+
+            Response.Flush();
+            Response.End();
+
+            return View(); // Optional: You can return a view or redirect if needed
+        }
+    }
+
+    
+}
